a92044446d1d9b5e75c50acdbe5c567f45c0f3b5
Anverze <gustavo@contaazul.com>
2015-03-19 17:38:08 -0300

    Criado funcao inItemText; excluido linhas desnecessarias; alterado nome da div principal.

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 function addNewItem(list, itemText) {
-	totalItems++;
-
-	var listItem = document.createElement("li");
-	
+	var listItem = document.createElement("li");	
 	var checkBox = document.createElement("input");
 	checkBox.type = "checkbox";
-	checkBox.id = "cb_" + totalItems;
-
 	var span = document.createElement("span");
-	span.id = "item_" + totalItems;
 	span.innerText = itemText;
 
 	listItem.appendChild(checkBox);
@@ -16,20 +10,19 @@ function addNewItem(list, itemText) {
 	list.appendChild(listItem);
 }
 
-var totalItems = 0;
-var inItemText = document.getElementById("inItemText");
-
 var btnNew = document.getElementById("btn");
 btnNew.onclick = function() {
-	var itemText = inItemText.value;
-	addNewItem(document.getElementById("todoList"), itemText);
-};
+	itemTextFunction();
+}
 
-
+var inItemText = document.getElementById("inItemText");
 inItemText.onkeyup = function(event) {
 	if(event.which == 13) {
-		var itemText = inItemText.value;
-		addNewItem(document.getElementById("todoList"), itemText);
+		itemTextFunction();
 	}
+}
 
+function itemTextFunction() {
+	var itemText = inItemText.value;
+	addNewItem(document.getElementById("todoList"), itemText);
 }
